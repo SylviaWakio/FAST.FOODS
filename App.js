da5c9eab90a14fe80941f3eb5ea6c70e8f9be162
+ <+>
2024-08-01 12:11:43 +0300

    done

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Food/navbar';
+import Home from './Food/Home';
+import Footer from './Food/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
